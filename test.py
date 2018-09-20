d6b9210c4dcc58d6cfdf1623888dbbb761a296cf
Abel Antunes <abel.neto@ccc.ufcg.edu.br>
2018-09-20 10:51:08 -0300

    Adicionando configuracoes iniciais

---
+++ test.py
@@ -0,0 +1,3 @@
+Creating lit-bastion-5032 in organization heroku... done, stack is cedar-14
+http://lit-bastion-5032.herokuapp.com/ | https://git.heroku.com/lit-bastion-5032.git
+Git remote heroku added
