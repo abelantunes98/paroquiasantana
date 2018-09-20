7764563edc184424d5de820263ef9c46d3bf0798
Abel Antunes <abel.neto@ccc.ufcg.edu.br>
2018-09-20 11:35:09 -0300

    aaa nnn

--- autoGit.sh
+++ autoGit.sh
@@ -2,7 +2,7 @@ git add .
 echo -n "Digite o comentario: "
 
 read comentario                                    
-git commit -m $comentario
+git commit -m "$comentario"
 git push
 
 echo "Commit feito!"
